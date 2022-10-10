95d5c905aec63524f93a04a7c70a5cd5ad1e3256
RafaelClaudio7 <rc7true@gmail.com>
2022-10-10 19:31:21 -0300

    objects pratice

--- src/A01-type-anotations/a4-object.ts
+++ src/A01-type-anotations/a4-object.ts
@@ -11,6 +11,18 @@ const objA: {
 };
 
 
+const bike: {
+    readonly price: number;
+    readonly name: string;
+} = {
+    price: 500,
+    name: 'caloi 300'
+};
+
+// bike.name = 'monark';
+console.log(bike.name + ' ' + bike.price)
+
+
 objA.keyA = 'Other value';
 console.log(objA.keyA);
 
