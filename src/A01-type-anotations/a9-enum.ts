12d9ebcc22ca7cc2d030867237e6e544568098eb
RafaelClaudio7 <rc7true@gmail.com>
2022-10-13 22:15:35 -0300

    enum

---
+++ src/A01-type-anotations/a9-enum.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+enum Colors {
+    RED,  // 0
+    BLUE, // 1
+    YELLOW, // 2
+}
+
+enum Colors {
+    DARKGREY = 0,  // 0
+    LIGHTBLUE = 1, // 1
+    WHITE = 15,
+    PINK // 2
+}
+
+console.log(Colors[0]);
