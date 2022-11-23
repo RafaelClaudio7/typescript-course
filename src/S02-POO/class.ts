44988cb2e9bf94c1512b0af79df77d27e5bad78f
RafaelClaudio7 <rc7true@gmail.com>
2022-11-22 22:29:15 -0300

    Class and Poo

---
+++ src/S02-POO/class.ts
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+export class Empresa {
+    public readonly nome: string;
+    private readonly colaboradores: Colaborador[] = [];
+    protected readonly cnpj: string;
+
+    constructor(nome: string, cnpj: string) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    adicionaColaborador(colaborador: Colaborador): void {
+        this.colaboradores.push(colaborador);
+    }
+}
+
+export class Colaborador {
+    constructor(public readonly nome: string, public readonly sobrenome: string) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+
+const colaborador1 = new Colaborador('Rafael', 'Claudio');
+const colaborador2 = new Colaborador('José', 'Quintão');
+const colaborador3 = new Colaborador('Sara', 'Mendes');
+
+
+const empresa1 = new Empresa('Facebook', '146.101.776-92');
+
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+
+console.log(empresa1);
