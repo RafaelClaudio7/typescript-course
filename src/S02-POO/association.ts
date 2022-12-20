5d92ad3fb5cce5828b25066ae2b4ade8b0c43a14
RafaelClaudio7 <rc7true@gmail.com>
2022-12-20 17:43:02 -0300

    classes associations

---
+++ src/S02-POO/association.ts
@@ -0,0 +1,51 @@
+/* eslint-disable */
+
+export class Escritor {
+  private _ferramenta: Ferramenta | null = null;
+
+  constructor(private _nome: string) {}
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this.ferramenta === null) {
+      console.log('Não posso escrever sem Ferramenta');
+      return;
+    }
+    this.ferramenta?.escrever();
+  }
+}
+
+export abstract class Ferramenta {
+  constructor(private _nome: string) {}
+
+  abstract escrever(): void;
+
+  get nome(): string {
+    return this._nome;
+  }
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está escrevendo...`);
+  }
+}
+
+export class MáquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Rafael');
+const caneta = new Caneta('Bic');
+
+escritor.ferramenta = caneta;
+escritor.escrever();
