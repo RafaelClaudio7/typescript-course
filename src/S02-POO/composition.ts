3a8e2778afb2481da9ac67c27711e9bb333751b9
RafaelClaudio7 <rc7true@gmail.com>
2022-12-20 18:40:04 -0300

    composition classes

---
+++ src/S02-POO/composition.ts
@@ -0,0 +1,45 @@
+/* eslint-disable */
+export class Carro {
+  private readonly motor: Motor = new Motor();
+
+  ligar(): void {
+    this.motor.ligar();
+  }
+
+  acelerar(): void {
+    this.motor.acelerar();
+  }
+
+  parar(): void {
+    this.motor.parar();
+  }
+
+  desligar(): void {
+    this.motor.desligar();
+  }
+}
+
+export class Motor {
+  ligar(): void {
+    console.log('O motor está ligado...');
+  }
+
+  acelerar(): void {
+    console.log('O motor está acelerando...');
+  }
+
+  parar(): void {
+    console.log('O motor está parado...');
+  }
+
+  desligar(): void {
+    console.log('O motor está desligado...');
+  }
+}
+
+
+const carro = new Carro();
+carro.ligar();
+carro.acelerar();
+carro.parar();
+carro.desligar();
