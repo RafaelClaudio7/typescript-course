41a03772642a6d89a8dd3d60a7b4fed1f73b37e2
RafaelClaudio7 <rc7true@gmail.com>
2022-12-21 15:22:37 -0300

    classes com type aliases

---
+++ src/S02-POO/class-type-alias.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+type TipoPessoa = {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto(): string;
+};
+
+class Pessoa implements TipoPessoa {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
